72363d46ed31298752c85ef5c8331b55434ebc08
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-09-11 03:16:39 +0700

    local

--- application/controllers/Image.php
+++ application/controllers/Image.php
@@ -1,6 +1,6 @@
 
 <?php
-use League\Flysystem\Adapter\LocalFilesytemAdapter as Local;
+use League\Flysystem\Local\LocalFilesytemAdapter as Local;
 use League\Flysystem\Filesystem;
 use League\Glide\ServerFactory;
 
