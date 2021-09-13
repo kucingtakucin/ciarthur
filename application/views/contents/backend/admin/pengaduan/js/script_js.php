d9436179476f45f397d7c88547d9e63d9cbca1a3
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-09-13 20:56:21 +0700

    pindah pusher

--- application/views/contents/backend/admin/pengaduan/js/script_js.php
+++ application/views/contents/backend/admin/pengaduan/js/script_js.php
@@ -1,17 +1,4 @@
 <script>
     $(() => {
-        var pusher = new Pusher('21a14c9bc94b57c3db03', {
-            cluster: 'ap1'
-        });
-
-        var channel = pusher.subscribe('ciarthur-pengaduan-channel');
-            channel.bind('ciarthur-pengaduan-event', function(data) {
-            console.log(JSON.stringify(data));
-            Swal.fire({
-                title: 'Informasi', 
-                icon: 'info', 
-                text: data.message
-            })
-        });
     })
 </script>
--- application/views/templates/backend/app.php
+++ application/views/templates/backend/app.php
@@ -282,6 +282,24 @@
         $(document).ready(function() {
 
             /**
+            * Keperluan pusher pengaduan
+            */
+            // ================================================== //
+            var pusher = new Pusher('21a14c9bc94b57c3db03', {
+                cluster: 'ap1'
+            });
+
+            var channel = pusher.subscribe('ciarthur-pengaduan-channel');
+                channel.bind('ciarthur-pengaduan-event', function(data) {
+                console.log(JSON.stringify(data));
+                Swal.fire({
+                    title: 'Informasi', 
+                    icon: 'info', 
+                    text: data.message
+                })
+            });
+
+            /**
             * Keperluan show preloader
             */
             // ================================================== //
