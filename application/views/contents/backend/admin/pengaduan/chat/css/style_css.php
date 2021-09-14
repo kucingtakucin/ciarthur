8163ed5c1c13cb84890e2a5a8279a980ac5057d8
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-09-14 10:32:38 +0700

    ini baru bener

---
+++ application/views/contents/backend/admin/pengaduan/chat/css/style_css.php
@@ -0,0 +1,9 @@
+<style>
+    .chat-right-aside {
+        -webkit-box-flex: 0;
+        -ms-flex: 0 0 100% !important;
+        flex: 0 0 100% !important;
+        max-width: 100% !important;
+        overflow: hidden;
+    }
+</style>
--- application/views/contents/backend/admin/pengaduan/css/style_css.php
+++
@@ -1,9 +0,0 @@
-<style>
-    .chat-right-aside {
-        -webkit-box-flex: 0;
-        -ms-flex: 0 0 100%;
-        flex: 0 0 100%;
-        width: 100% !important;
-        overflow: hidden;
-    }
-</style>
