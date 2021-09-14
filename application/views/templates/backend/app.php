4c4a10c2a1888ec80ac671b7a341696233cdf9ca
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-09-14 09:42:37 +0700

    udah bener ding

--- application/views/templates/backend/app.php
+++ application/views/templates/backend/app.php
@@ -61,7 +61,6 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
 
     <!-- Custom Stylesheets -->
-    <link rel="stylesheet" href="<?= base_url() ?>assets/cuba/css/style.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
     <?= $this->load->view($style, '', true) ?>
 </head>
